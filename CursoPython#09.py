005e8d2de7d75dff0f4e6894bbfacd21dfa25d7a
Mesquita <juanmesquita27@gmail.com>
2026-07-13 23:12:54 -0300

    Update CursoPython#09.py

--- CursoPython#09.py
+++ CursoPython#09.py
@@ -1,2 +1,8 @@
 frase = "Let's try this"
-print(frase[0:15:2])
+print("1", frase[0:15:2])
+print("2", frase[0:15])
+print("3", frase[:5])
+print("4", frase[0:5])
+print("5", frase[3:12:2])
+print("6", frase[::])
+print("7", frase[3:4:7])
